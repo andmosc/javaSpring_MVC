16a8b9642e21acbdf96b772fdbb92d6207ba0841
Andrey M <Moskovets.And@gmail.com>
2023-01-11 23:07:47 +0900

    added new "feature/removed"

--- src/main/java/ru/andmosc/service/PostService.java
+++ src/main/java/ru/andmosc/service/PostService.java
@@ -1,13 +1,15 @@
 package ru.andmosc.service;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import ru.andmosc.controller.PostController;
 import ru.andmosc.exception.NotFoundException;
 import ru.andmosc.model.Post;
 import ru.andmosc.repository.PostRepository;
+import ru.andmosc.repository.PostRepositoryStubImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class PostService {
     private final PostRepository repository;
@@ -15,15 +17,24 @@ public class PostService {
     public PostService(PostRepository repository) {
         this.repository = repository;
     }
+
     public List<Post> all() {
-        return repository.all();
+        return repository.all().stream().filter(p -> !p.isRemoved()).collect(Collectors.toList());
     }
+
     public Post getById(long id) throws NotFoundException {
-        return repository.getById(id).orElseThrow(() -> new NotFoundException(PostController.ID_NOT_FOUND));
+        Post post = repository.getById(id).orElseThrow(() -> new NotFoundException(PostRepositoryStubImpl.ID_NOT_FOUND));
+        if (!post.isRemoved()) {
+            return post;
+        } else {
+            throw new NotFoundException(PostRepositoryStubImpl.ID_NOT_FOUND);
+        }
     }
+
     public Post save(Post post) {
         return repository.save(post);
     }
+
     public void removeById(long id) {
         repository.removeById(id);
     }
